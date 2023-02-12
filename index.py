b77132acec120545ff33ef8d97ab1c97489a50b7
piwer-m <120250862+msludos@users.noreply.github.com>
2023-02-12 23:14:05 +0300

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/api/vk', methods=['POST'])
+@app.route('/api/vk')
 def vkapi():
     data = json.loads(request.data)
 
